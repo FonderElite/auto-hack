5aea3775c1cbda5646a0dcedce1f1120fbe45b0f
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-02 21:43:23 +0000

    Update gobuster.sh

--- gobuster/gobuster.sh
+++ gobuster/gobuster.sh
@@ -6,12 +6,12 @@ then
 read -p "URL: " url
 read -p "Wordlist-location: " wordlist
 read -p "Output File: " file
-gobuster dir -u $url  -w $wordlist -v -t 40 -o $file
+gobuster dir -u $url  -w $wordlist --no-error  -t 40 -s "204,301,302,307" -o $file
 else
 sudo apt update
 sudo apt install gobuster
 read -p "URL: " url_else
 read -p "wordlist-location: " wordlist_wordl
 read -p "Output File: " file
-gobuster dir -u $url_else  -w $wordlist_wordl -v -t 40 -o $file
+gobuster dir -u $url_else  -w $wordlist_wordl --no-error  -t 40 -s "204,301,302,307" -o $file
 fi
