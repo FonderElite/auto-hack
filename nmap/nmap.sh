facdfb7ebf6d9fff22c78311b68426bf40aa0467
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-02 18:19:25 +0000

    Update nmap.sh

--- nmap/nmap.sh
+++ nmap/nmap.sh
@@ -19,12 +19,42 @@ read -p "Choice: " choice
 if [ "$choice" -eq 1 ]
  then
   nmap -sV -sC $ip  -oN $file
-elif [ "$choice" -q 2 ]
+elif [ "$choice" -eq 2 ]
 then 
  nmap -sT $ip -oN $file
-elif [ "$choice" -q 3 ]
+elif [ "$choice" -eq 3 ]
 then 
  nmap -sS $ip -oN $file
+elif [ "$choice" -eq 4 ]
+then 
+nmap -sU $ip -oN $file
+elif [ "$choice" -eq 5 ]
+then 
+echo -e "
+Options:
+Option 1:Null
+Option 2:FIN
+Option 3:Xmas Scans
+"
+read -p "Choice: " prefer
+if [ "$prefer" -eq 1 ]
+then 
+nmap -sN $ip -oN $file
+elif [ "$prefer" -eq 2 ]
+then nmap -sF $ip -oN $file 
+elif [ "$prefer" -eq 3 ] 
+then nmap -sX $ip -oN $file 
+fi
+elif [ "$choice" -eq 6 ]
+then 
+nmap -sA $ip -oN $file
+elif [ "$choice" -eq 7 ]
+then 
+read -p "Enter Zombie IP: " zombie
+nmap -sI $zombie $ip -oN $file
+elif [ "$choice" -eq 8 ]
+then
+nmap -sS -T4 $ip --script firewall-bypass -oN $file
  fi
 else
  echo "Not a valid option"
