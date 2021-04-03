2323d3be40940a55aa55c075de85e723c5d86c30
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-03 18:14:40 +0000

    Update nikto.sh

--- nikto/nikto.sh
+++ nikto/nikto.sh
@@ -11,18 +11,96 @@ echo "Nikto Vulnerability Scanner
 "
 fi
 }
-
-options() {
-echo "
-Option 1: Basic Scan
-Option 2: 
-"
-}
 function main() {
 if [ -f "$NIKTO" ]
 then
 banner
+echo "
+Option 1: Basic Scan[0]
+Option 2: Scan Tuning[1]
+"
 read -p  "IP to scan: " ip
+read -p "Output file-name: " output
+read -p "Choice: " choice
+if [[ $choice -eq 0 ]]; then
+echo "Standard Nikto Scanning"
+ nikto -h $ip -output $output
+
+ if [ -f "$output" ] 
+then 
+echo "Scan Saved to $output"
+fi
+elif [ "$choice" -eq 1 ] 
+then
+echo "Scan Tuning Nikto"
+echo "
+Option 1:File Upload[0]
+Option 2:Interesting File[1]
+Option 3:Misconfiguration[2]
+Option 4:Injection(XSS/Script)[3]
+Option 5:Remote File Retrieval(Inside Web root)[4]
+Option 6:DOS[5]
+Option 7:Remote File Retrieval(Server Wide)[6]
+Option 8:Command Execution[7]
+Option 9:SQL Injection[8]
+"
+sleep 1
+read -p "Scan Tuning Option: " option
+fi 
+if [[ $option -eq 0 ]];then
+echo "File Upload Tuning"
+nikto -h $ip -Tuning 0  -output $output
+elif [ "$option" -eq 1 ]
+then
+echo "Interesting File Tuning"
+nikto -h $ip -Tuning 1 -output $output
+elif [ "$option" -eq 2 ]
+then 
+echo "Misconfiguration" 
+nikto -h $ip -Tuning 2  -output $output
+
+
+elif [ "$option" -eq 3 ]
+then
+echo "Injection(XSS/Script)"
+nikto -h $ip -Tuning 3 -output $output
+
+elif [ "$option" -eq 4 ]
+then
+echo "Remote File Retrieval(Inside Web root)"
+nikto -h $ip -Tuning 4 -output $output
+
+elif [ "$option" -eq 5 ]
+then
+echo "Denial Of Service"
+nikto -h $ip -Tuning 5 -output $output
+
+elif [ "$option" -eq 6 ]
+then
+echo "Remote File Retrieval(Server Wide)"
+nikto -h $ip -Tuning 6 -output $output
+
+elif [ "$option" -eq 7 ]
+then
+echo "Command Execution"
+nikto -h $ip -Tuning 7 -output $output
+
+elif [ "$option" -eq 8 ]
+then
+echo "SQL Injection"
+nikto -h $ip -Tuning 8 -output $output
+
+#else
+#echo "Standard Nikto Scan"
+#nikto -h $ip  -output $output
+fi
+else
+echo "Invalid Option
+===========================
+Doing the Standard Nikto Scan
+"
+nikto -h $ip -output $output
 fi
 }
+
 main
