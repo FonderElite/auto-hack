7361dee1377ceee24c09e6b934f20a8bdf931b24
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-10 10:48:57 +0000

    Update rustscan.sh

--- rustscan/rustscan.sh
+++ rustscan/rustscan.sh
@@ -16,8 +16,19 @@ then
 elif [ -f "$FILE_DEB" ] && [ ! -f "$FILE" ]
 then
  sudo dpkg -i $FILE_DEB
+if [ -f "$FILE" ] 
+then
+ read -p "Enter Ip Address: " ip
+    read -p "Scan Output File: " outfile
+    rustscan -b 2000 -a $ip --ulimit 5000 -- -sV -sC -oN $outfile
+fi    
+if [ -f "$OUTPUT" ]
+     then
+     echo  "Scan saved at $outfile"
+    fi
 else
 echo "Both of the files are missing."
 sleep 1
 echo "/usr/bin/rustscan and .deb file"
 fi
+       
