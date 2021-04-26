899aedfa891a294fb52338fd565054524d15a524
FonderElite <71190959+FonderElite@users.noreply.github.com>
2021-04-26 02:07:17 +0000

    Update sqlmap.sh

--- sqlmap/sqlmap.sh
+++ sqlmap/sqlmap.sh
@@ -17,12 +17,27 @@ Github:FonderElite
 
 function booleanBased() {
 echo "Boolean Based Technique" 
+read -p "Do you have cookies?: (y=1/n=0): " cookiein
 read -p "Url: " url1
 read -p "Data used in post req: " data1
 read -p "Testable Parameter(ex. search): " p1
 read -p "Level: " lvl1
 read -p "Risk: " risk1
-sqlmap --url="$url1" --method=POST --data="$data1" -p "$p1" -a --level=$lvl1 --risk=risk1 --flush-session --technique=B
+if [ "$cookiein" -eq 1 ]
+then
+read -p "Cookie: " cookie1
+sqlmap --url="$url1" --method=POST --data="$data1" --cookie="$cookie1" -p "$p1" -a --level=$lvl1 --risk=$risk1 --flush-session --technique=B
+else
+sqlmap --url="$url1" --method=POST --data="$data1" -p "$p1" -a --level=$lvl1 --risk=$risk1 --flush-session --technique=B
+fi
+}
+function errorBased() {
+echo "Error-Based Technique"
+read -p "Url: " url2
+read -p "Data used in post req: " data2
+read -p "Level: " lvl2
+read -p "Risk: " risk2
+sqlmap --url="$url2" --method=POST --data="$data2" -p "search" -a --level=$lvl2 --risk=$risk2 --flush-session --technique=E
 }
 function choices() {
 echo "
@@ -47,7 +62,10 @@ case $choice in
 1)
  booleanBased
  ;;
- esac
+2)
+ errorBased
+ ;; 
+esac
 
 }
 
