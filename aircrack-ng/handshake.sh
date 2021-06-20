45b53b9310658c54cf40ab91d7da94f7f2038bf8
FonderElite <s1170044@usls.edu.ph>
2021-06-20 05:45:12 +0000

    Update handshake.sh

--- aircrack-ng/handshake.sh
+++ aircrack-ng/handshake.sh
@@ -6,7 +6,7 @@ echo "Grabbing the handshake from target."
 read -p "Interface: " iface
 read -p "Channel of the AP: " channel
 read -p "BSSID: " bssid
-read -p "File to write: " file
+read -p "File-name to write: " file
 read -p "Do you wish to start now?(y/n): " choice
 if [[ $choice == "y" ]];then
 sudo airodump-ng --bssid $bssid -c $channel --write $file $iface
